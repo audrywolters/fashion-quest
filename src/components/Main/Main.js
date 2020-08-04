33b74784fbd568d67b056812e665983a46149cf5
Audry Wolters <audrywolters@gmail.com>
2020-08-04 18:50:09 -0500

    delted stuff I didn't need and created a Main page/component  where the action lives

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from "../LogOutButton/LogOutButton";
 
-class UserPage extends Component {
-
-  // this component doesn't do much to start, just renders some user info to the DOM
+class Main extends Component {
+  
   render() {
     return (
-      <div>
+      <>
+        <LogOutButton className="log-in" />
         <h1 id="welcome">Welcome, {this.props.user.username}!</h1>
         <p>Your ID is: {this.props.user.id}</p>
-        <LogOutButton className="log-in" />
-      </div>
+      </>
     );
   }
 }
 
 // Instead of taking everything from state, we just want the user info.
-const mapStateToProps = (state) => ({
-  user: state.user,
+const mapStateToProps = ( state ) => ({
+  user: state.user
 });
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserPage);
+export default connect( mapStateToProps )( Main );
