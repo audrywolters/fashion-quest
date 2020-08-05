90203cdefb73234c7130eed48fe5abb125fd2e29
Audry Wolters <audrywolters@gmail.com>
2020-08-05 18:15:56 -0500

    both story and input are printing inside of story but need to be shuffled or other

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,56 +1,86 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Senario from '../Senario/Senario';
 import  '../Story/Story.css';
 
 class Story extends Component {
   
     state = {
-        senarioList: []
+        senarioSoFar: [],
+        senarioCount: 0,
+        userInputSoFar: [],
+        userInputCount: 0
     }
 
     componentDidMount() {
+      // store all the story items
+      this.props.dispatch({ type: 'FETCH_SENARIO' });
     }
     
-    // catch when new input is entered
-    componentDidUpdate ( prevProps ) {
+    componentDidUpdate( prevProps ) {
+
+      // catch when new input is entered
       if ( prevProps.reduxState.input !== this.props.reduxState.input ) {
-        
+        // AUDRY - maybe for shuffle, we can decide what happens here
+        // aka whatShallHappen(userInput) -->?
+        this.updateUserInput();
+
         this.whatShallHappenNext();
       }
     }
 
-    whatShallHappenNext = () => {
-        console.log('hi it caught it');
+    updateUserInput = () => {
+      // inputs are created on the fly, so collect as we go along
+      let nextUserInput = this.props.reduxState.input;
+
+      // put this is a var - not really a shallow copy, right? but wtvr
+      let stateUserInputList = this.state.userInputSoFar;
+
+      // yay new div w newly entered text
+      let newUserInputDiv = <div key={ this.state.userInputCount }>{ nextUserInput }</div>;
+
+      // set for later!
+      this.setState({
+        userInputSoFar: stateUserInputList.concat( newUserInputDiv ),
+        userInputCount: this.state.userInputCount + 1
+      });
+    }
+    
+    updateSenario = () => {
+      // get the senario next in line
+      let nextSenario = this.props.reduxState.senarioList[ this.state.senarioCount ];
+
+      // put this is a var - not really a shallow copy, right? but wtvr
+      let stateSenarioList = this.state.senarioSoFar;
+      
+      // yay new div w newly entered text
+      let newSenarioDiv = <div key={ this.state.senarioCount }>{ nextSenario }</div>;
+
+      // set for later!
+      this.setState({
+        senarioSoFar: stateSenarioList.concat( newSenarioDiv ),
+        senarioCount: this.state.senarioCount + 1
+      });
     }
 
+    whatShallHappenNext = () => {
 
+      // at some point in here, we'll have to do this
+      this.updateSenario();  
 
-    // this is very silly and can only be prompted by
-    // a user event 
-    // it should fire after an input is inputed
-    // or for the begining, componentDidMount....
-    // what if they reload???
-    printSenario = () => {
-       
-        // prepare to add a new senario to the DOM
-        // make vars easy to read
-        const senarioList = this.props.reduxState.senarioList;
-        const length = this.props.reduxState.senarioList.length - 1;
+      // if input === 'closet' etc
 
-        this.setState({
-            senarioList: senarioList,
-            currentSenario: senarioList[ length ]
-        });
-
-        console.log('curr senar',  this.state.currentSenario );
+      // trigger next story
+      // if input === done or next or something
+      // get the next item in senarioList
     }
     
     render() {    
         return (
             <div className="storyBox">
-                <div>{ this.state.currentSenario }</div>
-                <button onClick={ this.printSenario }>Add a Senario!</button>
-                <div>{ this.props.reduxState.input }</div>
+                {/* we've got to tangle these up */}
+                <div>{ this.state.senarioSoFar }</div>
+                <div>{ this.state.userInputSoFar }</div>
             </div>
         );
     }
