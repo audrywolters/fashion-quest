ac23848a0765004c0405fbd6fc178c900cb98f67
Audry Wolters <audrywolters@gmail.com>
2020-08-04 22:26:01 -0500

    pants and shirts and stuff

--- database.sql
+++ database.sql
@@ -6,7 +6,9 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "closet" integer REFERENCES closet(id),
+    "storyState" integer REFERENCES story(id)
 );
 
 CREATE TABLE "closet" (
@@ -19,6 +21,48 @@ CREATE TABLE "story" (
     "senario" VARCHAR (1000) NOT NULL
 );
 
+
+CREATE TABLE "shirt" (
+    "id" SERIAL PRIMARY KEY,
+    "clothingType" integer DEFAULT 1,
+    "icon" VARCHAR (1000)  DEFAULT '-u-',
+    "fit" VARCHAR (50) NOT NULL,
+    "color" VARCHAR (50) NOT NULL,
+    "neck" VARCHAR (50) NOT NULL,
+    "sleeveLength" VARCHAR (50) NOT NULL
+);
+
+
+CREATE TABLE "pant" (
+    "id" SERIAL PRIMARY KEY,
+    "clothingType" integer DEFAULT 2,
+    "icon" VARCHAR (1000) DEFAULT '| |',
+    "fit" VARCHAR (50) NOT NULL,
+    "color" VARCHAR (50) NOT NULL,
+    "cut" VARCHAR (50) NOT NULL,
+    "legLength" VARCHAR (50) NOT NULL
+);
+
+
+
+
+
+INSERT INTO shirt 	("fit", 		"color", 					"neck", 		"sleeveLength")
+VALUES 				('loose', 		'grey', 					'scoop neck', 	'cap sleeve'), 
+	 				('tight', 		'black', 					'turtleneck', 	'long'), 
+	 				('baggy', 		'white', 					'cardigan', 	'knuckles'), 
+	 				('fitted', 		'orange pattern', 			'peterpan', 	'sleevless'), 
+	 				('fitted', 		'white', 					'button up', 	'short'), 
+	 				('normals', 	'grey', 					'ring', 		'short');
+	 				
+INSERT INTO pant 	("fit", 		"color", 					"cut", 			"legLength")
+VALUES 				('tight', 		'jeans', 					'slim', 		'heel'), 
+	 				('normals', 	'red with white stripe', 	'70s', 			'thigh'), 
+	 				('fitted', 		'darkwash jeans', 			'slim', 		'heel'), 
+	 				('loose', 		'purple', 					'bellbottoms', 	'ankle'), 
+	 				('sad tight', 	'black', 					'slacks', 		'high rise'), 
+	 				('saggy', 		'acid wash jeans', 			'bootcut', 		'heel');
+		
 INSERT INTO story ("senario")
 VALUES 	('steve says hi'), 
 		('lets go do stuff'), 
