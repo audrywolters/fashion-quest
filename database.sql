e724707d72de256ee0109ea3a473797699c89da2
Audry Wolters <audrywolters@gmail.com>
2020-08-04 21:53:50 -0500

    just a few new things

--- database.sql
+++ database.sql
@@ -8,3 +8,21 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "closet" (
+	"id" SERIAL PRIMARY KEY,	
+	"clothingItem" int
+);
+
+CREATE TABLE "story" (
+    "id" SERIAL PRIMARY KEY,
+    "senario" VARCHAR (1000) NOT NULL
+);
+
+INSERT INTO story ("senario")
+VALUES 	('steve says hi'), 
+		('lets go do stuff'), 
+		('look at that!'),
+		('what a cool hat'), 
+		('where am i'), 
+		('who are you and what is that');
