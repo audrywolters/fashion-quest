113a1ae17e906e14b7be9c8a3a8fb03a502b7dfb
Audry Wolters <audrywolters@gmail.com>
2020-08-15 16:38:01 -0500

    this is behind. hope not bad times ahead. but i added rejectUnauthenticated

--- server/routes/senario.router.js
+++ server/routes/senario.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 // get the all the story senarios
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     pool.query( `SELECT senario
                  FROM story
                  ORDER BY id;` )
